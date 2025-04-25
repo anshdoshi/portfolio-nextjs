b27908579101224974db6977ce5462446a05f478
Ansh Doshi <>
2025-04-26 03:33:55 +0530

    changes added

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -4,14 +4,22 @@ import HeroContent from "../sub/HeroContent";
 const Hero = () => {
   return (
     <div className="relative flex flex-col h-full w-full" id="about-me">
-      {/* <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
-      >
-        <source src="/blackhole.webm" type="video/webm" />
-      </video> */}
+      {/* {typeof window !== "undefined" && window.innerWidth >= 768 ? ( */}
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="rotate-180 absolute top-[-340px] h-full w-full left-0 z-[1] object-cover"
+          preload="auto"
+        >
+          <source src="/blackhole.webm" type="video/webm" />
+          {/* Fallback */}
+          {/* Your browser does not support the video tag. */}
+        </video>
+      {/* ) : (
+        <div className="absolute top-0 left-0 w-full h-full bg-black z-[1]" />
+      )} */}
       <HeroContent />
     </div>
   );
